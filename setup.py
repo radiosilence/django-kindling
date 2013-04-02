f7f52fed2fb22a4e46c416821b3b8760a3b2d2a9
James Cleveland <jc@blackflags.co.uk>
2013-04-02 12:33:37 +0100

    Distribution related

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+requires = []
+dep_links = []
+
+for dep in open('requirements.txt').read().split("\n"):
+    if dep.startswith('git+'):
+        dep_links.append(dep)
+    else:
+        requires.append(dep)
+
+setup(
+    name='django-kindling',
+    version="0.1",
+    description='Create nice new Django projects.',
+    long_description=open('README.rst').read(),
+    url='https://github.com/radiosilence/django-kindling',
+    author='James Cleveland',
+    author_email='jc@blackflags.co.uk',
+    packages=find_packages(),
+    include_package_data=True,
+    license="LICENSE.txt",
+    install_requires=requires,
+    dependency_links=dep_links,
+)
